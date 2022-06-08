e5e9e6af01ff5dd081e7b26790fff34d81cc29e4
PlanetDuTurfu <84854833+PlanetDuTurfu@users.noreply.github.com>
2022-06-08 16:35:59 +0200

    Add files via upload

---
+++ GUI/Started/PanelAnimations.java
@@ -0,0 +1,11 @@
+package Goorch.GUI.Started;
+
+import Goorch.Controleur;
+import javax.swing.JPanel;
+
+public class PanelAnimations extends JPanel{
+    public PanelAnimations(Controleur c)
+    {
+        this.setOpaque(false);
+    }
+}
